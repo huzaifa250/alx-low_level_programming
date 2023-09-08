60cb9371fbddbf30a40c13795a7e776cc9411789
huzaifa250 <huz.dark1@gmail.com>
2023-09-07 18:08:24 -0700

    coooo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - main function start the program
+ * return 0 when sucess
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
