3dccfb342d8eb95cf089593a3526f37f24902dc8
huzaifa250 <huz.dark1@gmail.com>
2023-09-08 06:02:24 -0700

    coooo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: C program Use the function printf
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("/""with proper grammar, but the outcome is a piece of art,");
+rerturn (0);
+}
