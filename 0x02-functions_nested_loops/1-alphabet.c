9db9e14ddcdfe2a978c907e6326b4775ecc90567
huzaifa250 <huz.dark1@gmail.com>
2023-09-12 13:01:15 -0700

    coooo

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet - Prints alphabet using _putchar function.
  *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
@@ -16,4 +15,3 @@ void print_alphabet(void)
 	_putchar('\n');
 	}
 }
-
