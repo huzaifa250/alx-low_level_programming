357dad6b3420a07cc1cfd9baf735f3676a4ff816
huzaifa250 <huz.dark1@gmail.com>
2023-09-21 05:42:55 -0700

    task 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -31,14 +31,3 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
-int main(void)
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	return (0);
-}
