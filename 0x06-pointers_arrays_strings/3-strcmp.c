4fad99d335921359d5f1a523e92d649702947ac2
huzaifa250 <huz.dark1@gmail.com>
2023-09-21 14:06:01 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,20 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 == *s2)
 	{
-		if (*s1 < *s2)
+		if (*s1 == '\0' && *s2 == '\0')
 		{
-			return (-1);
+			return (0);/*both are equal*/
 		}
-		else if (*s1 > *s2)
-		{
-			return (1);
-		}
-
 		s1++;
 		s2++;
 	}
 
-	return (0); /*Both are equal*/
+	return (*s1 - *s2); /*indicates the different*/
 }
