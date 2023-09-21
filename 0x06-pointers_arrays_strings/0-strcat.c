281c085cf703d65d38115ca4ee3cc2b55c04f9e7
huzaifa250 <huz.dark1@gmail.com>
2023-09-21 03:09:49 -0700

    edit task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = 0, j = 0;
+	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 	{
@@ -26,5 +27,6 @@ char *_strcat(char *dest, char *src)
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
 }
