89f178fddc3bbe02bf0204c3e7c55a985814ffa3
huzaifa250 <huz.dark1@gmail.com>
2023-10-02 16:30:25 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments pass to it(recive)
+ * @argc: number of arguments
+ * @argv: array or of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
