aa8873dc02039aa4c8073b9ef2812a170ff2ca0e
huzaifa250 <huz.dark1@gmail.com>
2023-10-02 16:02:38 -0700

    task 0

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main - Entry Point
+*Description: C program write the char c using _putchar
+*
+*Return: Always 0 (success)
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of arguments or list
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
