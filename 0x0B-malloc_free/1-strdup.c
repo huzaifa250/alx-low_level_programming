ed93ba5aa70a74d1eb30349ac992faa26e983f6c
huzaifa250 <huz.dark1@gmail.com>
2023-10-04 15:31:55 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,20 +13,20 @@ char *_strdup(char *str)
 	int i = 0, size = 0;
 	char *cp;
 
-	if (str == null)
-		return (null);
+	if (str == NULL)
+		return (NULL);
 
 	for (; str[size] != '\0'; size++)
 	;
 	cp = malloc(size * sizeof(*str) + 1);
 
 	if (cp == 0)
-		return (null);
+		return (NULL);
 	else
 	{
 		for (; i < size; i++)
 			cp[i] = str[i];
 	}
 
-	return (m);
+	return (cp);
 }
