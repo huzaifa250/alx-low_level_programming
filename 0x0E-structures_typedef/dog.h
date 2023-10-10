b6a7e4764312ad1b370e30de6c1cffa6151aec03
huzaifa250 <huz.dark1@gmail.com>
2023-10-10 16:56:04 -0700

    task  0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,37 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name(var instance)
+ * @age: dog's age(var instance2)
+ * @owner: dog's owner(var instance3)
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
