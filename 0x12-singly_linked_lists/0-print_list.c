dbb112b7410e93f3a7c0dc64df38f792688f66f9
huzaifa250 <huz.dark1@gmail.com>
2023-10-19 15:26:48 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t st = 0;
+	size_t i = 0;
 
 	while (h)
 	{
@@ -18,8 +18,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		st++;
+		i++;
 	}
 
-	return (st);
+	return (i);
 }
