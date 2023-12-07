d4eb2486779a668f89a6cb38395a09c1834d4f64
huzaifa250 <huz.dark1@gmail.com>
2023-12-07 08:36:20 -0800

    task 0,1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints elements of double linked list (dlistint_t list).
+ * @h: address of the head of the dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - calculate the length of  linked dlistint_t.
+ * @h: The head of the dlistint_t.
+ *
+ * Return: The number of elements in the dlistint_t list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node++
+	}
+	return (node);
+}
